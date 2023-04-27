f5b039ee912b7f4251a8f66e29e671b7abcb783c
Campbell Tedtsen <ctedtsen@gmail.com>
2023-04-26 21:00:53 -0400

    chat corrections

--- stevens-dining/client/src/components/Chat.js
+++ stevens-dining/client/src/components/Chat.js
@@ -1,149 +1,148 @@
 import React, {useEffect, useRef, useState} from 'react';
 import io from 'socket.io-client';
 
-function App() {
-  const [state, setState] = useState({message: '', name: ''});
-  const [chat, setChat] = useState([]);
-  const [previousRoom, setPreviousRoom] = useState("");
-  const [newRoom, setNewRoom] = useState("");
+function Chat() {
+const [state, setState] = useState({message: '', name: ''});
+const [chat, setChat] = useState([]);
+const [previousRoom, setPreviousRoom] = useState("");
+const [newRoom, setNewRoom] = useState("");
 
-  const socketRef = useRef();
+const socketRef = useRef();
 
-  useEffect(() => {
-    socketRef.current = io('/');
-    return () => {
-      socketRef.current.disconnect();
-    };
-  }, []);
-
-  useEffect(() => {
-    //console.log('join room')
-    setChat([]);
-    socketRef.current.emit("join-room",{
-      newRoom: newRoom,
-      previousRoom: previousRoom
-    });
-
-    socketRef.current.on("joined-room", (roomId) => {
-      setNewRoom(roomId);
-    })
-  }, [newRoom, previousRoom])
-
-  useEffect(() => {
-    socketRef.current.on('receive-message', ({name, message}) => {
-      console.log('The server has sent some data to all clients');
-      setChat([...chat, {name, message}]);
-    });
-    socketRef.current.on('user_join', function (data) {
-      setChat([
-        ...chat,
-        {name: 'ChatBot', message: `${data} has joined the chat`},
-      ]);
-    });
-
-    return () => {
-      socketRef.current.off('send-message');
-      socketRef.current.off('user-join');
-    };
-  }, [chat]);
-
-  const userjoin = (name) => {
-    socketRef.current.emit('user_join', name);
+useEffect(() => {
+  socketRef.current = io('http://localhost:4000');
+  return () => {
+    socketRef.current.disconnect();
   };
+}, []);
 
-  const onChangeHandler = () => {
-    setChat([]);
-    let room = document.getElementById('room-selector');
-    setPreviousRoom(newRoom);
-    setNewRoom(room.value);
-  }
+useEffect(() => {
+  setChat([]);
+  socketRef.current.emit("join-room",{
+    newRoom: newRoom,
+    previousRoom: previousRoom
+});
 
-  const onMessageSubmit = (e) => {
-    let msgEle = document.getElementById('message');
-    let room = document.getElementById('room-selector');
+socketRef.current.on("joined-room", (roomId) => {
+  setNewRoom(roomId);
+})
+}, [newRoom, previousRoom])
 
-    //room change
-    if(room.value !== newRoom){
-      setPreviousRoom(newRoom);
-      setNewRoom(room.value);
-    }
-    //console.log([msgEle.name], msgEle.value);
-    setState({...state, [msgEle.name]: msgEle.value});
-    socketRef.current.emit('send-message', {
-      name: state.name,
-      message: msgEle.value,
-      room: newRoom
-    });
-    e.preventDefault();
-    setState({message: '', name: state.name});
-    msgEle.value = '';
-    msgEle.focus();
-  };
+  useEffect(() => {
+    socketRef.current.on('receive-message', ({name, message}) => {
+      console.log('The server has sent some data to all clients');
+      setChat([...chat, {name, message}]);
+    });
+    socketRef.current.on('user_join', function (data) {
+      setChat([
+        ...chat,
+        {name: 'ChatBot', message: `${data} has joined the chat`},
+      ]);
+    });
 
-  const renderChat = () => {
-    return chat.map(({name, message}, index) => (
-      <div key={index}>
-        <h3>
-          {name}: <span>{message}</span>
-        </h3>
-      </div>
-    ));
-  };
+    return () => {
+      socketRef.current.off('send-message');
+      socketRef.current.off('user-join');
+    };
+  }, [chat]);
 
-  return (
-    <div>
-      {state.name && (
-        <div className='card-chat'>
-          <div className='render-chat'>
-            <h1>Chat Log</h1>
-            {renderChat()}
-          </div>
-          <form onSubmit={onMessageSubmit}>
-            <h1>Messenger</h1>
-            <div>
-              <select id="room-selector" onChange = {onChangeHandler}>
-                <option value="">General</option>
-                <option value="sono">Sono</option>
-              </select>
-              <input
-                name='message'
-                id='message'
-                variant='outlined'
-                label='Message'
-              />
-            </div>
-            <button>Send Message</button>
-          </form>
+  const userjoin = (name) => {
+    socketRef.current.emit('user_join', name);
+  };
+
+  const onMessageSubmit = (e) => {
+    let msgEle = document.getElementById('message');
+    let room = document.getElementById('room-selector');
+
+    //room change
+    if(room.value !== newRoom){
+      setPreviousRoom(newRoom);
+      setNewRoom(room.value);
+    }
+    //console.log([msgEle.name], msgEle.value);
+    setState({...state, [msgEle.name]: msgEle.value});
+    socketRef.current.emit('send-message', {
+      name: state.name,
+      message: msgEle.value,
+      room: newRoom
+    });
+    e.preventDefault();
+    setState({message: '', name: state.name});
+    msgEle.value = '';
+    msgEle.focus();
+  };
+
+  const onChangeHandler = () => {
+    setChat([])
+    let room = document.getElementById("room-selector");
+    setPreviousRoom(newRoom);
+    setNewRoom(room.value);
+  }
+
+  const renderChat = () => {
+    return chat.map(({name, message}, index) => (
+      <div key={index}>
+        <h3>
+          {name}: <span>{message}</span>
+        </h3>
+      </div>
+    ));
+  };
+
+return (
+  <div>
+    {state.name && (
+      <div className='card-chat'>
+        <div className='render-chat'>
+          <h1>Chat Log</h1>
+          {renderChat()}
         </div>
-      )}
-
-      {!state.name && (
-        <form
-          className='form'
-          onSubmit={(e) => {
-            console.log(document.getElementById('username_input').value);
-            e.preventDefault();
-            setState({name: document.getElementById('username_input').value});
-            userjoin(document.getElementById('username_input').value);
-            // userName.value = '';
-          }}
-        >
-          <div className='form-group'>
-            <label>
-              User Name:
-              <br />
-              <input id='username_input' />
-            </label>
+        <form onSubmit={onMessageSubmit}>
+          <h1>Messenger</h1>
+          <div>
+            <select id="room-selector" onChange={onChangeHandler}>
+              <option value="">General</option>
+              <option value="music">Music</option>
+            </select>
+            <input
+              name='message'
+              id='message'
+              variant='outlined'
+              label='Message'
+            />
           </div>
-          <br />
-
-          <br />
-          <br />
-          <button type='submit'> Click to join</button>
+          <button>Send Message</button>
         </form>
-      )}
-    </div>
-  );
+      </div>
+    )}
+
+    {!state.name && (
+      <form
+        className='form'
+        onSubmit={(e) => {
+          console.log(document.getElementById('username_input').value);
+          e.preventDefault();
+          setState({name: document.getElementById('username_input').value});
+          userjoin(document.getElementById('username_input').value);
+          // userName.value = '';
+        }}
+      >
+        <div className='form-group'>
+          <label>
+            User Name:
+            <br />
+            <input id='username_input' />
+          </label>
+        </div>
+        <br />
+
+        <br />
+        <br />
+        <button type='submit'> Click to join</button>
+      </form>
+    )}
+  </div>
+  );
 }
 
-export default App;
+export default Chat;
--- stevens-dining/client/src/components/NavBar.js
+++ stevens-dining/client/src/components/NavBar.js
@@ -11,28 +11,28 @@ function NavigationBar() {
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">   
               <Link className="nav-link" to="/" id="link-txt">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">   
-              <Link className="nav-link" to="/locations" id="link-txt">
                 All Locations
               </Link>
             </li>
+            <li className="nav-item">   
+                <Link className="nav-link" to="/chat" id="link-txt">
+                  Chat
+                </Link>
+            </li>
           </ul>
-            <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <div className="dropdown">
+                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Dining Locations
                 </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item" href="/piercecafe">Pierce Cafe</a>
-                    <a class="dropdown-item" href="/america">America's Cup</a>
-                    <a class="dropdown-item" href="/gateway">Gateway Cafe</a>
-                    <a class="dropdown-item" href="/pierce">Pierce Dining Hall</a>
-                    <a class="dropdown-item" href="/grill">Grill Nation</a>
-                    <a class="dropdown-item" href="/halal">Halal Stall</a>
-                    <a class="dropdown-item" href="/italia">Piccola Italia</a>
-                    <a class="dropdown-item" href="/sono">Sono</a>
+                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a className="dropdown-item" href="/piercecafe">Pierce Cafe</a>
+                    <a className="dropdown-item" href="/america">America's Cup</a>
+                    <a className="dropdown-item" href="/gateway">Gateway Cafe</a>
+                    <a className="dropdown-item" href="/pierce">Pierce Dining Hall</a>
+                    <a className="dropdown-item" href="/grill">Grill Nation</a>
+                    <a className="dropdown-item" href="/halal">Halal Stall</a>
+                    <a className="dropdown-item" href="/italia">Piccola Italia</a>
+                    <a className="dropdown-item" href="/sono">Sono</a>
                 </div>
             </div>
         </div>
